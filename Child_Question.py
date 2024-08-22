5f9cb41dcdf2beada689e2cc185f3b3921aec292
Rahib-Khan <rahib.khandaker@gmail.com>
2024-08-22 12:26:05 -0400

    updates to college question

--- Child_Question.py
+++ Child_Question.py
@@ -3,7 +3,16 @@ import streamlit_survey as ss
 
 survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
 
-st.write("What College do you go to?")
+
+st.write("What Type of Aid Are you looking for?")
+aid = survey.radio("Issues",
+    options=["Food Insecurity", "Housing Instability", "Child Care", "Career Development", "Disability Services", "Mental Health", "Addiction Services", "Health And Wellness"],
+    index= None,
+    label_visibility="collapsed",
+    horizontal=True,
+)
+if aid== "Childcare":
+    st.write("What College do you go to?")
 college = survey.radio("College",
             options = [
     "Baruch College",
--- First.py
+++ First.py
@@ -7,7 +7,7 @@ survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
 st.write("What Type of Aid Are you looking for?")
 aid = survey.radio("Issues",
     options=["Food Insecurity", "Housing Instability", "Child Care", "Career Development", "Disability Services", "Mental Health", "Addiction Services", "Health And Wellness"],
-    index=0,
+    index=None,
     label_visibility="collapsed",
     horizontal=True,
 )
